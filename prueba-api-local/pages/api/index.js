4b9592c2fbb8286d0fdf2ca9e9be743193054c24
Andres2310 <and.23108@gmail.com>
2022-08-23 18:15:48 -0400

    add mongoose

---
+++ prueba-api-local/pages/api/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const mongoose=require('mongoose')
+const mongoDB=process.env.DATABASE_URL
+const Model=require("../../model/model")
+
+
+mongoose.connect(mongoDB);
+export default async function handler(req, res) {
+  
+  if(req.method==='POST'){
+    res.status(200).json({response:"todo ok"})
+
+  }else if(req.method==='GET'){
+    try {
+      const data = await Model.find();
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+  
+
+}
